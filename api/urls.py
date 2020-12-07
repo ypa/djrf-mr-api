a3095fe396b2519da2add4b78868f8946122a952
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-06 20:37:44 -0800

    Register api routes for movie and rating view sets

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

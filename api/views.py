7165261afd4dae4b5d72715b4292c8f373018faa
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-08 20:26:30 -0800

    Add rate_movie endpoint

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -7,6 +10,29 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    @action(detail=True, methods=['POST'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            # user = request.user  # will use after auth setup is done
+            user = User.objects.get(id=1)  # hard coded user for now
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except Rating.DoesNotExist:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'You need to provide stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()

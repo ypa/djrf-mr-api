2dd2e961be3ceef55a9357312a2c86922b987b5c
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-09 20:26:26 -0800

    Also create auth token when user gets created

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from .models import Movie, Rating
 
 
@@ -11,6 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
 
